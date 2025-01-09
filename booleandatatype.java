e0d299c53503d6cf4afbe2f28fea09948b9b737a
thesiyadiya <diyapatel9412@gmail.com>
2025-01-09 17:42:37 +0530

    Add files via upload

---
+++ booleandatatype.java
@@ -0,0 +1,17 @@
+class boolean1
+{
+    public static void main(String p[])
+    {
+        boolean a,b;
+        a=true;
+        b=false;
+        System.out.println("value:"+a);
+        System.out.println("value:"+b);
+    }
+}
+
+
+//PS C:\diya\java> javac booleandatatype.java
+//PS C:\diya\java> java boolean1
+//value:true
+//value:false
