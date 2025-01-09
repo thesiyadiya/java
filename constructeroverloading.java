1297d093f6f2c42a0472c4e4714814449d31ed71
thesiyadiya <diyapatel9412@gmail.com>
2025-01-09 17:02:36 +0530

    constructeroverloading.java

--- constructeroverloading.java
+++ constructeroverloading.java
@@ -13,4 +13,19 @@ class spr2
         spr2 a=new spr2();
         a=new spr2(s[0]);
     }
-}
+} 
+
+
+//PS C:\diya\java> javac constructeroverloading.java
+//PS C:\diya\java> java spr2 diya
+//THIS IS SPR2 CLASS
+//NAME= diya
+
+//PS C:\diya\java> javac constructeroverloading.java
+//PS C:\diya\java> java spr2 diya thesiya
+//THIS IS SPR2 CLASS
+//NAME= diya
+//PS C:\diya\java> javac constructeroverloading.java
+//PS C:\diya\java> java spr2 "diya thesiya"
+//THIS IS SPR2 CLASS
+//NAME= diya thesiya 
