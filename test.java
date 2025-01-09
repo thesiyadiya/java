aacb385705fd7db2929b5b516e8738f61cde8813
thesiyadiya <diyapatel9412@gmail.com>
2025-01-09 17:14:17 +0530

    test.java

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
-class Demo
+class test
 {
     public static void main(String s[])
     {
         System.out.print("This is Demo");
     }
 }
+
+
+//PS C:\diya\java> javac test.java
+//PS C:\diya\java> java test
+//This is DemoPS C:\diya\java>
