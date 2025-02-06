e118e6b8be4dc638e5420d9b7a6428bcb2a965c3
thesiyadiya <diyapatel9412@gmail.com>
2025-02-06 15:16:39 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,12 @@
+class demo
+{
+   public static void main(String s[])
+   {
+        System.out.println("This is Demo");
+   } 
+}
+ /*
+PS C:\diya\java> javac demo.java
+PS C:\diya\java> java demo
+This is Demo
+*/
